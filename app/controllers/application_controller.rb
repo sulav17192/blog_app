eef3b7a31fe6d2bf1713fe5c1c06e641fb8ba4e6
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-13 15:45:06 +0545

    updated application_controller, new.html.erb, edit.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_keys = [:first_name, :last_name]
+    # For sign up (registration)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For account update (edit profile)
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
