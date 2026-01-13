645b9324553c59f1c8faf7fa90d81671f4111269
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-13 16:58:38 +0545

    created post model and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_13_102048) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_13_111306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "confirmation_sent_at"
@@ -31,4 +40,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_13_102048) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
