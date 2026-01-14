4fff693bf03a86ff525a63c2dbef3e45724ea759
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-14 17:07:04 +0545

    fixed enum error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  enum role: { reader: 0, author: 1, admin: 2 }
+ enum :role, { reader: 0, author: 1, admin: 2 }
 
   has_many :posts, dependent: :destroy
 end
