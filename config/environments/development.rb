3486b46997fc75738d7bddf13966d2b0f628206a
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-14 23:16:27 +0545

    used smtp for confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,14 +31,27 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
 
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"],
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
+
+
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
