4d782bb7926e85aed56c99abf542f56a6de6ba4d
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-14 14:44:59 +0545

    created migration for add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_13_111306) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_14_085835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_13_111306) do
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.integer "role"
     t.string "unconfirmed_email"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
